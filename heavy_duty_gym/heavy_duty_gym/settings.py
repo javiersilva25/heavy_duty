aae6998914dadba20d56afd7d8690a12f09dc46c
Javier <133884608+javiersilva25@users.noreply.github.com>
2024-06-18 13:03:45 -0400

    Add files via upload

--- heavy_duty_gym/heavy_duty_gym/settings.py
+++ heavy_duty_gym/heavy_duty_gym/settings.py
@@ -76,9 +76,12 @@ WSGI_APPLICATION = 'heavy_duty_gym.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'heavy_duty_gym',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'}
 }
 
 
